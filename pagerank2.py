0e01377d6ff4e6e845a16b37f014d9b7669a18e5
dman996 <d.man.996@gmail.com>
2017-12-17 18:13:24 -0600

    PageRank 2 is for use on the vol and weighted matrices that are now used

---
+++ pagerank2.py
@@ -0,0 +1,36 @@
+##Standard PageRank implementation
+##Requires a square matrix
+import numpy as np
+import scipy as sc
+import pandas as pd
+from fractions import Fraction
+# keep it clean and tidy
+
+# we have 50 webpages and probability of landing to each one is 1/50
+#(defaultProbability)
+dp = Fraction(1,50)
+# penalty
+beta = 0.7
+maxerr = .0001
+hlm = np.loadtxt(open("./hyperlinkMatrix.csv", "rb"), delimiter=",")
+A = np.matrix(hlm)
+E = np.zeros(A.shape)
+E[:] = .02
+#A = beta * A + ((1-beta) * E)
+r = np.zeros((A.shape[0],1))
+print(np.sum(A,axis=0))
+r[:] = .02
+#r = r.T
+print(r.shape)
+prev_r = r
+
+for i in range(1,100):
+    print("iteration: \n",i)
+    r = np.dot(A, r)
+    #check if converged
+    if (prev_r-r<maxerr).all():
+        break
+    prev_r = r
+
+print ("Final:\n", r)
+print ("sum", np.sum(r))
